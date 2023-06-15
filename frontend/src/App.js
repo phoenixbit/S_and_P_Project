f6251d5647027831c440e63457c64b041bd6f5c0
phoenixbit <90902951+phoenixbit@users.noreply.github.com>
2023-06-15 13:41:08 +0400

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import PEBox from "./PEBox"; 
+//
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get("http://localhost:5000/data");
+      setData(response.data);
+    };
+
+    fetchData();
+  }, []);
+
+  if (!data) return "Loading...";
+
+  return (
+    <div>
+      <PEBox pe={data.sp500_pe} data={data.data} />
+    </div>
+  );
+}
+
+export default App;
